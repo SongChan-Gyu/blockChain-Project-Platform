c9cb1ceb7f8092113a3bc2ab542fcd6930ce3f8e
SongChan-Gyu <zx2253@naver.com>
2019-06-06 16:36:54 +0900

    commit

--- deploy/image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-registry='localhost:5000'
-image='dockerfarm-frontend'
-
-docker build -t ${image} --no-cache .
-docker tag ${image} ${registry}/${image}:${BUILD_NUMBER}
-docker push ${registry}/${image}:${BUILD_NUMBER}
-docker rmi -f ${image} ${registry}/${image}:${BUILD_NUMBER}
